46a89d016f771d6252cd410e0110a841ed6315de
Ozair Couto <marloncouto719@gmail.com>
2025-04-30 18:15:21 -0300

    Replace header with Navbar component in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "./components/Navbar.jsx";
 
 export default function App() {
   return (
     <>
-      <h1>My Gastronomy</h1>
+      <Navbar />
     </>
   );
 }
