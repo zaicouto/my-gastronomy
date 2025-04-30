eaf76dbbed2b5689c2749c4ef77d5fb236a1478a
Ozair Couto <marloncouto719@gmail.com>
2025-04-30 20:19:46 -0300

    Implement routing with react-router-dom and add page components for Home, Cart, Profile, Meals, and Auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar.jsx";
+import { Outlet } from "react-router-dom";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Outlet />
     </>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,48 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home/page.jsx";
+import Cart from "./pages/cart/page.jsx";
+import Profile from "./pages/profile/page.jsx";
+import Meals from "./pages/meals/page.jsx";
+import Auth from "./pages/auth/page.jsx";
+
+const pages = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/meals",
+        element: <Meals />,
+      },
+      {
+        path: "/auth",
+        element: <Auth />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={pages} />
   </StrictMode>
 );
