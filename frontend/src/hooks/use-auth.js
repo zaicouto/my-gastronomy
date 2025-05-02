26811e0a2a1319dcad8d4eef7d87673df487c104
Ozair Couto <marloncouto719@gmail.com>
2025-05-02 18:45:52 -0300

    Add authentication hook and base URL constant for login and signup functionality

---
+++ frontend/src/hooks/use-auth.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { baseUrl } from "../utils/constants.js";
+
+export default function useAuth() {
+  const [loading, setLoading] = React.useState(false);
+
+  const login = async (formData) => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(`${baseUrl}/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+      console.log("Login response: ", data);
+
+      return data;
+    } catch (error) {
+      console.error("Error logging in: ", error);
+      return;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const logout = async () => {};
+
+  const signUp = async (formData) => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(`${baseUrl}/auth/signup`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+      console.log("Sign up response: ", data);
+
+      return data;
+    } catch (error) {
+      console.error("Error signin up: ", error);
+      return;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    login,
+    logout,
+    signUp,
+    loading,
+  };
+}
